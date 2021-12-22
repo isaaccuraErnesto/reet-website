92937a58fa6bafa817e3d1f00d92a4f7bdbad16f
Ernesto <isaaccura.ernesto@gmail.com>
2021-12-22 17:18:05 +1100

    Finished Services Section

--- src/components/Home/HomeFooterIcon.js
+++ src/components/Home/HomeFooterIcon.js
@@ -3,8 +3,8 @@ const HomeFooterIcon = ({id, BootstrapClassName, caption}) => {
         <div id={id} className='home-footer-icon d-flex justify-content-center align-items-center'>
             <figure className='home-footer-fig d-flex flex-column justify-content-center align-items-center'>
                 <div className='row fa-stack fa-3x'>
-                <i className="fas fa-circle fa-stack-2x"></i>
-                <i className={BootstrapClassName}></i>
+                    <i className="fas fa-circle fa-stack-2x"></i>
+                    <i className={BootstrapClassName}></i>
                 </div>
                 <figcaption className='row'>{caption}</figcaption>
             </figure>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,70 +1,90 @@
-import Images from './ServiceImageData'
-
-const Services = () => {
-    console.log(Images)
-    
+const Services = () => {    
     return (
-        <div id='services'>
-            <div id='services-images'>
-                <div id='top-left-img' style={{backgroundImage: `url(${Images[0].img})`}}></div>
-                <div id='top-right-img' style={{backgroundImage: `url(${Images[1].img})`}}></div>
-                <div id='bottom-right-img' style={{backgroundImage: `url(${Images[2].img})`}}></div>
-                <div id='bottom-left-img' style={{backgroundImage: `url(${Images[3].img})`}}></div>
-            </div>
-            <div id='services-text'>
-                <h3>Services</h3>
-                <div id='services-categories' className='d-flex align-items-center justify-content-around'>
-                    <div id='roofing'>
-                        <h4>Roofing</h4>
-                        <ul>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                        </ul>
-                    </div>
-                    <div id='carpentry'>
-                        <h4>Carpentry</h4>
-                        <ul>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                        </ul>
-                    </div>
-                    <div id='renovations'>
-                        <h4>Renovations</h4>
-                        <ul>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                        </ul>
-                    </div>
-                    <div id='handyman'>
-                        <h4>Handyman</h4>
-                        <ul>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                        </ul>
-                    </div>
-                    <div id='maintenance'>
-                        <h4>Maintenance</h4>
-                        <ul>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                            <li>Lorem</li>
-                        </ul>
-                    </div>
+        <div id='services' className='d-flex'>
+            <div id='services-roofing' className='service-box'>
+                <div className='service-icon row fa-stack fa-3x'>
+                    <i className='fas fa-circle fa-stack-2x'></i>
+                    <i className='fas fa-wrench fa-stack-1x fa-xs'></i>
                 </div>
+                <h4>Roofing</h4>
+                <hr style={{width: '50%'}}/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat blanditiis voluptas quo sunt maiores non molestias vel pariatur inventore ipsam.</p>
+                <hr style={{width: '50%'}}/>
+                <ul>
+                    <li>alsdalkdsj</li>
+                    <li>asdasd</li>
+                    <li>asdad asdasd</li>
+                    <li>asdasdasd</li>
+                    <li>asdasd asd</li>
+                </ul>
+            </div>
+            <div id='services-carpentry' className='service-box'>
+                <div className='service-icon row fa-stack fa-3x'>
+                    <i className='fas fa-circle fa-stack-2x'></i>
+                    <i className='fas fa-hammer fa-stack-1x fa-xs'></i>
+                </div>
+                <h4>Carpentry</h4>
+                <hr style={{width: '50%'}}/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat blanditiis voluptas quo sunt maiores non molestias vel pariatur inventore ipsam.</p>
+                <hr style={{width: '50%'}}/>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+            </div>
+            <div id='services-renovations' className='service-box'>
+                <div className='service-icon row fa-stack fa-3x'>
+                    <i className='fas fa-circle fa-stack-2x'></i>
+                    <i className='fas fa-paint-roller fa-stack-1x fa-xs'></i>
+                </div>
+                <h4>Renovations</h4>
+                <hr style={{width: '50%'}}/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat blanditiis voluptas quo sunt maiores non molestias vel pariatur inventore ipsam.</p>
+                <hr style={{width: '50%'}}/>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+            </div>
+            <div id='services-handyman' className='service-box'>
+                <div className='service-icon row fa-stack fa-3x'>
+                    <i className='fas fa-circle fa-stack-2x'></i>
+                    <i className='fas fa-toolbox fa-stack-1x fa-xs'></i>
+                </div>
+                <h4>Handyman</h4>
+                <hr style={{width: '50%'}}/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat blanditiis voluptas quo sunt maiores non molestias vel pariatur inventore ipsam.</p>
+                <hr style={{width: '50%'}}/>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+            </div>
+            <div id='services-maintenance' className='service-box'>
+                <div className='service-icon row fa-stack fa-3x'>
+                    <i className='fas fa-circle fa-stack-2x'></i>
+                    <i className='fas fa-screwdriver fa-stack-1x fa-xs'></i>
+                </div>
+                <h4>Maintenance</h4>
+                <hr style={{width: '50%'}}/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat blanditiis voluptas quo sunt maiores non molestias vel pariatur inventore ipsam.</p>
+                <hr style={{width: '50%'}}/>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
             </div>
         </div>
     )
